b6821e64cd92d3b466f2f65e424a6fc724fa36d0
Eben Ofere <46578091+ebenofere@users.noreply.github.com>
2019-11-22 11:35:09 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 let app = express();
-let configure = require("./server/configure");
-
-const PORT = process.env.PORT || 2027;
+let configure = require('./server/configure');
 
 app = configure(app);
 
-mongoose.connect("mongodb://localhost/authentication", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection.on(
-  "error",
-  console.error.bind(console, "Connection Error"),
-);
-mongoose.connection.once("open", () => {
-  console.log("Connected to Database...");
+// mongoose.connect("mongodb://localhost/authentication", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+
+// db.once("open", () => {
+//   console.log("Connected to Database...");
+// });
+
+// db.on("error", (err) => {
+//   console.log(err);
+// });
+
+app.get('/', (req, res) => {
+  res.render('contact');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server Listening on: ${PORT}`);
+app.post('/send', (req, res) => {
+  console.log(req.body);
+
+  res.render('contact', {msg:'Email has been sent'});
+
 });
+
+const PORT = process.env.PORT || 2027;
+
+app.listen(PORT, () => console.log(`Server Listening on: ${PORT}`));
